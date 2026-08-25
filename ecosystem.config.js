ac0e38d352f5b13d72da72281799aff50b638363
NgLeThaiVi <145008267+NgLeThaiVi@users.noreply.github.com>
2026-08-26 01:00:48 +0700

    Update ecosystem.config.js with cwd for PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: 'thieu-nhi-tan-thai-son-app', // Tên ứng dụng của bạn trên PM2 (đặt gì cũng được)
-      script: './app.js', 
-      instances: 'max', // Tự động dùng hết số nhân CPU của VPS
-      exec_mode: 'cluster', // Chạy chế độ đa luồng để tăng hiệu suất
-      max_memory_restart: '400M', // Nếu app bị tràn RAM vượt quá 400MB, PM2 sẽ tự restart để chống sập VPS
-      node_args: '--max-old-space-size=512', // Giới hạn cứng dung lượng V8 engine được dùng tối đa 512MB RAM
+      name: 'thieu-nhi-tan-thai-son-app',
+      script: './app.js',
+      cwd: '/var/www/Donbosco_Web', // <--- THÊM DÒNG NÀY ĐỂ PM2 ĐỨNG ĐÚNG THƯ MỤC DỰ ÁN
+      instances: 'max', 
+      exec_mode: 'cluster', 
+      max_memory_restart: '400M',
+      node_args: '--max-old-space-size=512',
       env: {
-        NODE_ENV: 'production', // Chuyển sang môi trường chạy thực tế
-        PORT: 3000, // Cổng nội bộ mà ứng dụng Node.js của bạn sẽ lắng nghe
+        NODE_ENV: 'production',
+        PORT: 3000,
       },
     },
   ],
