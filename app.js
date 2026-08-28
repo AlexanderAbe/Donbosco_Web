5b1994f4d2a46e583fd10cc4f7722b9ac64a04bc
NgLeThaiVi <145008267+NgLeThaiVi@users.noreply.github.com>
2026-08-28 07:56:09 +0700

    Them sitemap.xml

--- app.js
+++ app.js
@@ -35,6 +35,34 @@ app.use('/bdh', require('./src/routes/bdh-route'));
 app.use('/truong-khoi', require('./src/routes/truong-khoi-route'));
 app.use('/glv', require('./src/routes/glv-route'));
 
+// Route Sitemap.xml cho SEO Google
+app.get('/sitemap.xml', (req, res) => {
+    try {
+        const urls = [
+            { loc: 'https://thieunhitanthaison.com/', priority: '1.0' },
+            { loc: 'https://thieunhitanthaison.com/auth/login', priority: '0.8' },
+            { loc: 'https://thieunhitanthaison.com/auth/forgot-password', priority: '0.5' }
+        ];
+
+        let xml = '<?xml version="1.0" encoding="UTF-8"?>';
+        xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+        
+        urls.forEach(item => {
+            xml += '<url>';
+            xml += `<loc>${item.loc}</loc>`;
+            xml += `<priority>${item.priority}</priority>`;
+            xml += '</url>';
+        });
+        
+        xml += '</urlset>';
+
+        res.header('Content-Type', 'application/xml');
+        res.send(xml);
+    } catch (err) {
+        res.status(500).end();
+    }
+});
+
 // Trang chủ chuyển hướng về đăng nhập
 app.get('/', (req, res) => {
     res.redirect('/auth/login');
