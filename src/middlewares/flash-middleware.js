85ea9f6092e67cef34df9d22da9bd7f74a8bab99
NgLeThaiVi <145008267+NgLeThaiVi@users.noreply.github.com>
2026-09-17 17:09:08 +0700

    update flash message by using toast

--- src/middlewares/flash-middleware.js
+++ src/middlewares/flash-middleware.js
@@ -1,6 +1,14 @@
 module.exports = (req, res, next) => {
-    res.locals.session = req.session; 
+    res.locals.session = req.session;
     res.locals.success = req.session.successMessage;
+    res.locals.error = req.session.errorMessage;
+    res.locals.message = req.session.message;
+    res.locals.warning = req.session.warningMessage;
+    res.locals.info = req.session.infoMessage;
     delete req.session.successMessage;
+    delete req.session.errorMessage;
+    delete req.session.message;
+    delete req.session.warningMessage;
+    delete req.session.infoMessage;
     next();
 };
