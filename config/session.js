fef54ba645dcf699dad0ef9c01b4a47c03b04549
Abedeus <tranbao083@gmail.com>
2026-09-19 19:18:47 +0700

    toi uu giao dien bdh

--- config/session.js
+++ config/session.js
@@ -1,31 +1,31 @@
-const session = require('express-session');
-const pgSession = require('connect-pg-simple')(session);
-const { Pool } = require('pg');
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
+const { Pool } = require("pg");
 
 // Tạo một pool kết nối riêng cho session sử dụng biến môi trường DATABASE_URL
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false // Cần thiết khi kết nối với Supabase qua Cloud
-    }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Cần thiết khi kết nối với Supabase qua Cloud
+  },
 });
 
 module.exports = session({
-    store: new pgSession({
-        pool: pool,
-        tableName: 'session',
-        createTableIfMissing: true,
-        pruneSessionInterval: false
-    }),
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    proxy: true,
-    cookie: { 
-        maxAge: 30 * 60 * 1000, // 30 phút
-        secure: process.env.NODE_ENV === 'production',
-        httpOnly: true,
-        sameSite: 'lax',
-        domain: 'thieunhitanthaison.com'
-    } 
+  store: new pgSession({
+    pool: pool,
+    tableName: "session",
+    createTableIfMissing: true,
+    pruneSessionInterval: false,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  proxy: true,
+  cookie: {
+    maxAge: 30 * 60 * 1000, // 30 phút
+    secure: /*false*/ process.env.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite: "lax",
+    domain: "thieunhitanthaison.com",
+  },
 });
